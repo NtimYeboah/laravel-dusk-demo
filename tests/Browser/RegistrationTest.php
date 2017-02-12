4bf378ff48f4a59f73c64578ff72d16a73b58eb4
ntimobedyeboah <ntimyeboah.obed@callenssolutions.com>
2017-02-12 17:36:05 +0000

    Add test coverage for registering users

---
+++ tests/Browser/RegistrationTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\User;
+use Faker\Factory;
+use Illuminate\Support\Facades\DB;
+use Tests\DuskTestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class RegistrationTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_can_register()
+    {
+        $faker = Factory::create();
+
+        $this->browse(function($browser) use ($faker) {
+            $password = $faker->password(9);
+
+            $browser->visit('/register')
+                ->assertSee('Register')
+                ->type('name', $faker->name)
+                ->type('email', $faker->safeEmail)
+                ->type('password', $password)
+                ->type('password_confirmation', $password)
+                ->press('Register')
+                ->assertPathIs('/home')
+                ->assertSee('Yah, welcome to cool contacts');
+        });
+    }
+}
