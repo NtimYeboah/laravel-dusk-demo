85c821f8ae0f26d1986d941b71112e01d57b2cfc
ntimobedyeboah <ntimyeboah.obed@callenssolutions.com>
2017-02-12 17:25:42 +0000

    Add password field

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,6 +16,7 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
 
     return [
         'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail
+        'email' => $faker->unique()->safeEmail,
+        'password' => 'secret'
     ];
 });
